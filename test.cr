061e9d74c041c1614a1d9834deece879d74931a7
Robin Broda <robin@broda.me>
2018-12-09 01:46:13 +0100

    Test: Render string

--- test.cr
+++ test.cr
@@ -2,4 +2,15 @@
 
 require "./src/fontanyl"
 
-Fontanyl::BDF.new("font.bdf")
+font = Fontanyl::BDF.new("font.bdf")
+
+def gets(font : Fontanyl::Font, msg : String)
+  stra = Array(String).new(17) { "" } # hack
+  msg.chars.each do |c|
+    char = font.get(c)
+    char.bitmap.each_slice(char.width).with_index { |e, ei| e.each { |i| stra[ei] = stra[ei] + (i ? '#' : ' ') }; stra[ei] += "  " }
+  end
+  stra.join("\n")
+end
+
+puts gets(font, "TEST")
