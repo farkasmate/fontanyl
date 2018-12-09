1c17393369029123149bfacf39c3ef559e120af2
Robin Broda <robin@broda.me>
2018-12-09 01:44:26 +0100

    Font: Add useful properties

--- src/font.cr
+++ src/font.cr
@@ -3,9 +3,17 @@ require "./glyph"
 module Fontanyl
   class Font
     property charmap : Hash(Char, Glyph)
+    property family : String
+    property size : UInt32
 
     def initialize
       @charmap = Hash(Char, Glyph).new
+      @family = ""
+      @size = 0_u32
+    end
+
+    def get(c : Char) : Glyph
+      @charmap[c]
     end
   end
 end
